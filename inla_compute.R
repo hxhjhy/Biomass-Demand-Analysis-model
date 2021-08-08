761732c762b6ceaaffedeece3761ab7ecc6cef74
hxhjhy <39166651+hxhjhy@users.noreply.github.com>
2021-08-08 18:44:07 +0800

    Update inla_compute.R

--- inla_compute.R
+++ inla_compute.R
@@ -6,14 +6,13 @@ ene.inla <- function(name, formula_num, data){     #using formula_num to select 
   y <- data[paste("y0",name,sep = "_")]
   data$y <- y
   data$pro2 <- data$pro
-  formula <- model.selection(formula_num)
+  if (name == "wood") {formula = model.selection.wood(formula_num)}
+  else ( formula = model.selection.straw(formula_num))
   ene <- inla(formula, data = data,  
                  family = "gaussian",
                  control.predictor = list(compute = T),
                  control.compute = list(cpo = TRUE, dic = TRUE, waic = TRUE))      
 }
-
-
 
 
 
