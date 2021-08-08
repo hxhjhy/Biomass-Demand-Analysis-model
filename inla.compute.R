a51a802e26e6b5f04457c0e85f38634092c22022
hxhjhy <39166651+hxhjhy@users.noreply.github.com>
2021-08-08 17:33:50 +0800

    Update inla.compute.R

--- inla.compute.R
+++ inla.compute.R
@@ -2,11 +2,11 @@
 ## Model compute--------------------------------------------------
 
 ene.inla <- function(name, formula_num, data){     #using formula_num to select model
-  data <- data%>% rename(y = exp(paste("y0",name,sep = "_")))
-
+  
+  y <- data[paste("y0",name,sep = "_")]
+  data$y <- y
   data$pro2 <- data$pro
-  formula <- switch(formula_num, formula_1, formula_2, formula_3, formula_4,
-                    formula_5, formula_6)
+  formula <- model.selection(formula_num)
   ene <- inla(formula, data = data,  
                  family = "gaussian",
                  control.predictor = list(compute = T),
