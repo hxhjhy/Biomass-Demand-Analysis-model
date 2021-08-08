95edfc0bfc764efb3daf1575b5c0c7989bea1083
hxhjhy <39166651+hxhjhy@users.noreply.github.com>
2021-08-08 18:09:17 +0800

    Update model_selection.R

--- model_selection.R
+++ model_selection.R
@@ -2,47 +2,81 @@
 
 ## option 1-6
 
-mdoel.selection <- function(formula_num){
+mdoel.selection.straw <- function(formula_num){
 
-formula_1 <- y ~ f_pop +  f_size  + f_inc  + f_land + Ex_forest + 
-  Ex_road + Ex_coal + cr + year
+    formula_1 <- y ~ H_pop + H_inc  + P_land + P_forest + 
+      P_road + P_coal + P_straw + year
 
-formula_2 <- y ~ f_pop + I(f_pop^2) + I(f_pop^3) + 
-  f_size + I(f_size^2) + I(f_size^3) +
-  f_inc + I(f_inc^2) + I(f_inc^3) +
-  f_land + I(f_land^2) + I(f_land^3) +
-  Ex_forest + Ex_road + Ex_coal + cr + year
+    formula_2 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_road + P_coal + P_straw + year
 
-formula_3 <- y ~ f_pop + I(f_pop^2) + I(f_pop^3) + 
-  f_size + I(f_size^2) + I(f_size^3) +
-  f_inc + I(f_inc^2) + I(f_inc^3) +
-  f_land + I(f_land^2) + I(f_land^3) + pro
+    formula_3 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_road + P_coal + P_straw + pro
 
-formula_4 <- y ~ f_pop + I(f_pop^2) + I(f_pop^3) + 
-  f_size + I(f_size^2) + I(f_size^3) +
-  f_inc + I(f_inc^2) + I(f_inc^3) +
-  f_land + I(f_land^2) + I(f_land^3) +
-  Ex_forest + Ex_road + Ex_coal + cr +
-  f(pro, model = "besagproper", graph = wood.nb.rs) 
+    formula_4 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_road + P_coal + P_straw + pro +
+      f(pro, model = "besagproper", graph = wood.nb.rs) 
 
-formula_5 <- y ~ f_pop + I(f_pop^2) + I(f_pop^3) + 
-  f_size + I(f_size^2) + I(f_size^3) +
-  f_inc + I(f_inc^2) + I(f_inc^3) +
-  f_land + I(f_land^2) + I(f_land^3) +
-  Ex_forest + Ex_road + Ex_coal + cr +
-  f(year, model = "ar",order = 2) +
-  f(pro, model = "besagproper", graph = wood.nb.rs) 
+    formula_5 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_road + P_coal + P_straw +
+      f(year, model = "ar",order = 2) +
+      f(pro, model = "besagproper", graph = wood.nb.rs) 
 
-formula_6 <- y ~ f_pop + I(f_pop^2) + I(f_pop^3) + 
-  f_size + I(f_size^2) + I(f_size^3) +
-  f_inc + I(f_inc^2) + I(f_inc^3) +
-  f_land + I(f_land^2) + I(f_land^3) +
-  Ex_forest + Ex_road + Ex_coal + cr +
-  f(year, model = "ar",order = 2) +
-  f(pro, model = "besagproper", graph = wood.nb.rs) +
-  + f(pro2, year, model="iid", constr=TRUE)
+    formula_6 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_road + P_coal + P_straw +
+      f(year, model = "ar",order = 2) +
+      f(pro, model = "besagproper", graph = wood.nb.rs) +
+      + f(pro2, year, model="iid", constr=TRUE)
+
+    formula <- switch(formula_num, formula_1, formula_2, formula_3, formula_4,
+                       formula_5, formula_6)
+}
   
-formula <- switch(formula_num, formula_1, formula_2, formula_3, formula_4,
-                   formula_5, formula_6)
+mdoel.selection.wood <- function(formula_num){
+
+    formula_1 <- y ~ H_pop + H_inc  + P_land + P_forest + 
+       P_coal + year
+
+    formula_2 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_coal + year
+
+    formula_3 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_coal + pro
+
+    formula_4 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_coal + pro +
+      f(pro, model = "besagproper", graph = wood.nb.rs) 
+
+    formula_5 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_coal + f(year, model = "ar",order = 2) +
+      f(pro, model = "besagproper", graph = wood.nb.rs) 
+
+    formula_6 <- y ~ H_pop + I(H_pop^2) + I(H_pop^3) + 
+      H_inc + I(H_inc^2) + I(H_inc^3) +
+      P_land + I(P_land^2) + I(P_land^3) +
+      P_forest + P_coal + f(year, model = "ar",order = 2) +
+      f(pro, model = "besagproper", graph = wood.nb.rs) +
+      + f(pro2, year, model="iid", constr=TRUE)
+
+    formula <- switch(formula_num, formula_1, formula_2, formula_3, formula_4,
+                       formula_5, formula_6)
 }
   
