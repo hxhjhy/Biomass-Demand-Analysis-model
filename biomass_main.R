7a9c279d5f63bb98fe19ccafb5642a4ed8dba17d
hxhjhy <39166651+hxhjhy@users.noreply.github.com>
2021-08-08 19:11:33 +0800

    Update biomass_main.R

--- biomass_main.R
+++ biomass_main.R
@@ -65,8 +65,6 @@ wood.inla <- ene.inla("wood",6, group_wood)
 wood_result_province <- fitted_value(group_wood, wood.inla)[[1]]
 wood_result_nation <- fitted_value(group_wood, wood.inla)[[2]]
 
-round(wood.inla$summary.fitted.values$mean,3)
-
 ## straw -----------------------------------------------------------
 group_straw <-straw_process(df_data)[[1]]
 df_straw_nzero <- straw_process(df_data)[[2]]
@@ -84,62 +82,52 @@ sheets = list("wood_result_nation" = wood_result_nation,
 
 write.xlsx(sheets,"result_combined.xlsx")
 
-## Cross-validation--------------------------------------------------
-# wood
-valid_group_wood <- filter(group_wood,is.na(pre)) ## using group_wood to valid
-valid_obs_wood <- valid_obs("wood", df_wood_nzero)  ## using observation to valid
-
-#(1) group valid
-yvg.wood <- valid_test('year','wood',valid_group_wood)   #year trend
-pvg.wood <- valid_test('pro','wood',valid_group_wood)  # pro trend
-yvg.wood$obs.flag <- "wood"
-pvg.wood$group.flag <- "wood"
-yvg.wood <- yvg.wood %>% rename(true = y0_wood)
-pvg.wood <- pvg.wood %>% rename(true = y0_wood)
-#--------------------------------------------------------------
-# straw
-valid_group_straw <- filter(group_straw,is.na(pre)) ## using group_straw to valid
-valid_obs_straw <- valid_obs("straw",df_straw_nzero)  ## using observation to valid
-
-#(1) group valid
-yvg.straw <- valid_test('year','straw',valid_group_straw)   #year trend
-pvg.straw <- valid_test('pro','straw',valid_group_straw)  # pro trend
-yvg.straw$obs.flag <- "straw"
-pvg.straw$group.flag <- "straw"
-yvg.straw <- yvg.straw %>% rename(true = y0_straw)
-pvg.straw <- pvg.straw %>% rename(true = y0_straw)
-#--------------------------------------------------------------
-
 #model selection------------------------------------------------------------
 
 loocv <- function(fit){     ### to compute LCPO
   -2 * log(fit$cpo$cpo)
 }
+wood_dics <- list()
+wood_cpos <- list()
+straw_dics <- list()
+straw_cpos <- list()
 
-dics <- list()
-cpos <- list()
-
-for (n in seq(1,6)){
-  model.inla <- ene.inla("biogas",n,group_biogas)
-  dics[n] <- model.inla$dic$dic
-  cpos[n] <- sum(loocv(model.inla),na.rm=TRUE)
+for (n in seq(1,6)){  
+  mdoel.inla.straw <- ene.inla("straw",n,group_straw)
+  straw_dics[n] <- straw.inla.straw$dic$dic
+  straw_cpos[n] <- sum(loocv(model.inla.straw),na.rm=TRUE)
 }
 
-#model validation------------------------------------------------------------
-#obs.vld.wood <- valid_cross_test("wood", df_wood_nzero,1)
-#obs.vld.straw <- valid_cross_test("straw", df_straw_nzero,2)
-#obs.vld.biogas <- valid_cross_test("biogas", df_biogas_nzero,3)
-#obs.vld.ele <- valid_cross_test("ele", df_ele_nzero,3)
+for (n in seq(1,6)){  
+  model.inla.wood <- ene.inla("wood",n,group_wood)
+  wood_dics[n] <- model.inla.wood$dic$dic
+  wood_cpos[n] <- sum(loocv(model.inla.wood),na.rm=TRUE)
+}
 
-group.vld.wood <- valid_cross_test("wood", group_wood,1)
-group.vld.straw <- valid_cross_test("straw", group_straw,2)
-group.vld.biogas <- valid_cross_test("biogas", df_biogas_nzero,3)
-group.vld.ele <- valid_cross_test("ele", group_ele,4)
-
+## model validation-------------------------------------------------------
+# wood
+valid_group_wood <- filter(group_wood,is.na(pre)) ## using group_wood to valid
+cross_valid <- valid_cross_test('wood',valid_group_wood, 1) # five-folds to cross-validation
 group.vld.wood.m <- valid_machine_test("wood",group_wood, 1)
+#summary results of annually iterated or province iterated cross-validation for biomass consumption
+yvg.wood <- valid_test('year','wood',valid_group_wood)   #year trend
+pvg.wood <- valid_test('pro','wood',valid_group_wood)  # pro trend
+yvg.wood$group.flag <- "wood"
+pvg.wood$group.flag <- "wood"
+yvg.wood <- yvg.wood %>% rename(true = y0_wood)
+pvg.wood <- pvg.wood %>% rename(true = y0_wood)
+#---------------------------------------------------------------------------
+# straw
+valid_group_straw <- filter(group_straw,is.na(pre)) ## using group_straw to valid
+cross_valid <- valid_cross_test('straw',valid_group_straw, 2) # five-folds to cross-validation
 group.vld.straw.m <- valid_machine_test("straw",group_straw, 2)
-group.vld.biogas.m <- valid_machine_test("biogas",df_biogas_nzero, 3)
-group.vld.ele.m <- valid_machine_test("ele",group_ele, 4)
+yvg.straw <- valid_test('year','straw',valid_group_straw)   #year trend
+pvg.straw <- valid_test('pro','straw',valid_group_straw)  # pro trend
+yvg.straw$group.flag <- "straw"
+pvg.straw$group.flag <- "straw"
+yvg.straw <- yvg.straw %>% rename(true = y0_straw)
+pvg.straw <- pvg.straw %>% rename(true = y0_straw)
+
 
 #plot------------------------------------------------------------
 
